30d5ec3fbdb55f37600b2f519814e83fae3bd323
Pete Kim <pete.kim@coinbase.com>
2022-03-25 17:05:33 -0500

    chore: remove proxy deployment in the deploy script

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -6,43 +6,26 @@ async function main() {
   const { ethers } = hre;
 
   const deployer = (await ethers.getSigners())[0];
-
   const resolverFactory = await ethers.getContractFactory("CoinbaseResolver");
-  const proxyFactory = await ethers.getContractFactory("ERC1967Proxy");
 
-  console.log("deploying implementation contract...");
-  const implementation = await resolverFactory.deploy();
-  await implementation.deployed();
-  console.log("-> deployed implementation contract at", implementation.address);
-
-  console.log("initializing implementation contract with dummy values...");
-  await implementation.initialize(ethers.constants.AddressZero, "", []);
-  console.log("-> initialized implementation contract");
-
-  const iCoinbaseResolver = CoinbaseResolver__factory.createInterface();
-
-  const proxyArgs = [
-    implementation.address,
-    iCoinbaseResolver.encodeFunctionData("initialize", [
-      deployer.address,
-      "http://localhost:3000/r/{sender}/{data}",
-      [deployer.address],
-    ]),
+  const constructorArgs: [string, string, string, string, string[]] = [
+    deployer.address,
+    deployer.address,
+    deployer.address,
+    "http://localhost:3000/r/{sender}/{data}",
+    [deployer.address],
   ];
 
+  const iCoinbaseResolver = CoinbaseResolver__factory.createInterface();
+  const constructorData = iCoinbaseResolver.encodeDeploy(constructorArgs);
+
+  console.log(`deploying CoinbaseResolver (arguments: ${constructorData})...`);
+
+  const implementation = await resolverFactory.deploy(...constructorArgs);
   console.log(
-    `deploying proxy contract (arguments: ${JSON.stringify(proxyArgs)})...`
+    "-> deployed CoinbaseResolver contract at",
+    implementation.address
   );
-  const proxy = await proxyFactory.deploy(
-    implementation.address,
-    iCoinbaseResolver.encodeFunctionData("initialize", [
-      deployer.address,
-      "http://localhost:3000/r/{sender}/{data}",
-      [deployer.address],
-    ])
-  );
-  await proxy.deployed();
-  console.log("-> deployed proxy contract at", proxy.address);
 }
 
 main()
